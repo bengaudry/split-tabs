f2f122045a0ff224d527de0b3ae96509d680c0a4
bengaudry <bengaudry@outlook.fr>
2025-04-16 12:39:30 +0200

    Fix: Script & icons

--- pack_app.bat
+++ pack_app.bat
@@ -1,7 +1,9 @@
 @echo off
 del extension.zip
 cd src
-tar.exe -a -c -f extension.zip background.js styles.css manifest.json split-view.html split-view.js content-script.js ../icons
+xcopy /E /I ..\icons icons
+tar.exe -a -c -f extension.zip background.js styles.css manifest.json split-view.html split-view.js content-script.js icons
 move extension.zip ..
 cd ..
+rmdir /S /Q src\icons
 echo Extension files have been compressed into extension.zip
