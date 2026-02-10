86ba63125f00bc944c07f4184a47fcc0d314706b
bengaudry <bengaudry@outlook.fr>
2026-02-10 12:55:26 +0100

    refactor: moved FORBIDDEN_HOSTNAMES constant into a separate file to avoid duplication

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,17 @@
 export const IS_DEV = true;
 export const MIN_VIEW_PERCENTAGE = 30;
+
+export const FORBIDDEN_HOSTNAMES = [
+  "accounts-static.cdn.mozilla.net",
+  "accounts.firefox.com",
+  "addons.cdn.mozilla.net",
+  "addons.mozilla.org",
+  "api.accounts.firefox.com",
+  "content.cdn.mozilla.net",
+  "discovery.addons.mozilla.org",
+  "install.mozilla.org",
+  "oauth.accounts.firefox.com",
+  "profile.accounts.firefox.com",
+  "support.mozilla.org",
+  "sync.services.mozilla.com"
+];
--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,17 +1,4 @@
-export const FORBIDDEN_HOSTNAMES = [
-  "accounts-static.cdn.mozilla.net",
-  "accounts.firefox.com",
-  "addons.cdn.mozilla.net",
-  "addons.mozilla.org",
-  "api.accounts.firefox.com",
-  "content.cdn.mozilla.net",
-  "discovery.addons.mozilla.org",
-  "install.mozilla.org",
-  "oauth.accounts.firefox.com",
-  "profile.accounts.firefox.com",
-  "support.mozilla.org",
-  "sync.services.mozilla.com"
-];
+import { FORBIDDEN_HOSTNAMES } from "./constants";
 
 /** Returns an array of urls that are allowed to be opened in the split view */
 export function filterIncorrectTabs(
